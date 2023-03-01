6e1a31cf20aefbe93edab47437593773e7d2f1fd
mo7amedsayed74 <114395937+mo7amedsayed74@users.noreply.github.com>
2023-03-02 01:08:00 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Muhamed_sayEd74
+#include <bits/stdc++.h>
+#include "insertionSort.h"
+using namespace std;
+
+int main() {
+    /*
+       // to check insertion sort
+       
+       int arr[8]={9,2,7,5,1,4,3,6};
+       insertionSort(arr,8);
+       for(int i=0;i<8;i++)cout<<arr[i]<<' ';
+
+     */
+
+
+    return 0;
+}
