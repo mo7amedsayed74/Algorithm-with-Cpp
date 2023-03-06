c78f5ff4e35d1bacc25565059c46fd767a3d4afd
mo7amedsayed74 <114395937+mo7amedsayed74@users.noreply.github.com>
2023-03-06 06:06:59 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,29 @@
 using namespace std;
 
 int main() {
+    
     /*
        // to check insertion sort
        
-       int arr[8]={9,2,7,5,1,4,3,6};
-       insertionSort(arr,8);
-       for(int i=0;i<8;i++)cout<<arr[i]<<' ';
+       ////____ Increase ____////
+       int arr1[8]={9,2,7,5,1,4,3,6};
+       insertionSort_Increase_1(arr1,8);
+       for(int i=0;i<8;i++) cout<<arr1[i]<<' ';
+       cout<<'\n';
+       int arr2[8]={9,2,7,5,1,4,3,6};
+       insertionSort_Increase_2(arr2,8);
+       for(int i=0;i<8;i++) cout<<arr2[i]<<' ';
+       cout<<'\n';
+
+       ////____ Decrease ____////
+       int  arr3[8]= {11,2,7,6,1,20,3,5};
+       insertionSort_Decrease_1(arr3,8);
+       for(int i=0;i<8;i++) cout<<arr3[i]<<' ';
+       cout<<'\n';
+       int  arr4[8]= {11,2,7,6,1,20,3,5};
+       insertionSort_Decrease_2(arr4,8);
+       for(int i=0;i<8;i++) cout<<arr4[i]<<' ';
+       cout<<'\n';
 
      */
 
